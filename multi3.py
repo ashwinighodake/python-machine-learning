66dc5e6d47a3b7945b633a2410052b80d1f7bdc1
ashwinighodake <ashwini752000@gmail.com>
2022-01-24 15:16:12 +0530

    Performing multithreading using python

---
+++ multi3.py
@@ -0,0 +1,20 @@
+import os 
+
+def Fun():
+    print("Inside Fun")
+    print("PID is:",os.getpid())
+    for i in range(5):
+        print("Fun:",i)
+
+def Gun():
+    print("Inside Gun")
+    print("PID is:",os.getpid())
+    for i in range(5):
+        print("Fun:",i)
+    
+def main():
+    print("PID is",os.getpid())
+    Fun()
+    Gun()
+if __name__=="__main__":
+    main()
